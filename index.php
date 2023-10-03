9065670a74063df3ef57cae20396124d70dff07b
Akeops <lalanne.andoni1@gmail.com>
2023-10-03 14:17:34 +0200

    css en cours

--- index.php
+++ index.php
@@ -9,27 +9,26 @@ $articles = getArticles();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Les articles</title>
-    <link rel="stylsheet" href="css/app.css">
+    <link rel="stylesheet" href="css/app.css">
 </head>
+
 <body>
     <header>
-        <nav id="navbar">
+        <nav class="container">
             <ul>
                 <li><a href="#">Accueil</a></li>
                 <li><a href="#">Contact</a></li>
             </ul>
         </nav>
     </header>
-    <main>
-        <section id="about">
-            <div class="container">
-                <img src="https://picsum.photos/506/253" alt="a_propos">
-                <div>
-                    <h1>Titre 1</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam at turpis ut velit faucibus blandit. Phasellus sed dignissim sapien. Nam id ligula mollis, ultrices dolor a, aliquet ipsum. Fusce vitae sem tincidunt, egestas justo vel, mattis elit. Vestibulum feugiat at diam quis imperdiet. Aenean risus quam, lacinia ut hendrerit vitae, efficitur id nisl. Duis semper molestie condimentum. In at erat felis.</p>
-                </div>
+        <div class="container">
+            <img src="https://picsum.photos/506/253" alt="a_propos" class="about_image">
+            <div class="about_text">
+                <h1>Titre 1</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam at turpis ut velit faucibus blandit. Phasellus sed dignissim sapien. Nam id ligula mollis, ultrices dolor a, aliquet ipsum. Fusce vitae sem tincidunt, egestas justo vel, mattis elit. Vestibulum feugiat at diam quis imperdiet. Aenean risus quam, lacinia ut hendrerit vitae, efficitur id nisl. Duis semper molestie condimentum. In at erat felis.</p>
             </div>
-        </section>
+        </div>
+    <main>
         <section id="last-articles">
             <?php foreach ($articles as $article) { ?>
             <div class="container">
