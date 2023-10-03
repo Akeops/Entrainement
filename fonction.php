4417058df9426351ac2af62b661ba4d8428dd40c
Akeops <lalanne.andoni1@gmail.com>
2023-10-03 14:00:15 +0200

    Exercice php terminé

--- fonction.php
+++ fonction.php
@@ -1,2 +1,17 @@
+<?php 
+function getArticles(){
+    try {
+        $db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->exec('SET NAMES utf8');
 
+        $sql = "SELECT title, content, image  FROM articles ORDER BY id DESC LIMIT 3";
+        $query = $db->query($sql);
+        $articles = $query->fetchAll();
+        return $articles;
+    }
+    catch (PDOException $e) {
+        die('Erreur : ' . $e->getMessage()); 
+    }
+}
 
---
+++ index.php
@@ -0,0 +1,50 @@
+<?php 
+include_once("fonction.php"); 
+$articles = getArticles();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Les articles</title>
+    <link rel="stylsheet" href="css/app.css">
+</head>
+<body>
+    <header>
+        <nav id="navbar">
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section id="about">
+            <div class="container">
+                <img src="https://picsum.photos/506/253" alt="a_propos">
+                <div>
+                    <h1>Titre 1</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam at turpis ut velit faucibus blandit. Phasellus sed dignissim sapien. Nam id ligula mollis, ultrices dolor a, aliquet ipsum. Fusce vitae sem tincidunt, egestas justo vel, mattis elit. Vestibulum feugiat at diam quis imperdiet. Aenean risus quam, lacinia ut hendrerit vitae, efficitur id nisl. Duis semper molestie condimentum. In at erat felis.</p>
+                </div>
+            </div>
+        </section>
+        <section id="last-articles">
+            <?php foreach ($articles as $article) { ?>
+            <div class="container">
+                <img src="<?= $article['image'] ?>" ?>
+                <div>
+                    <?= $article["title"] ?>
+                    <?= $article["content"] ?>
+                </div>
+
+            </div>
+            <?php } ?>
+        </section>
+    </main>
+        
+
+    
+</body>
+</html>
